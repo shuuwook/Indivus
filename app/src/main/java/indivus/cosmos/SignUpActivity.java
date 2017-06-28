f22bee37e575422d7f2dcd18b4f93651bc719410
Jiyong-C-Lee <liramus214@gmail.com>
2017-06-28 19:23:21 +0900

    signupActivity 1차완성
    signupActivity 1차완성

--- app/src/main/java/indivus/cosmos/SignUpActivity.java
+++ app/src/main/java/indivus/cosmos/SignUpActivity.java
@@ -1,14 +1,15 @@
 package indivus.cosmos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import indivus.cosmos.presenter.SignupPresenter;
 
-import static indivus.cosmos.R.drawable.confirm;
 import static indivus.cosmos.R.id.signup_email_confirm;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -24,7 +25,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     Button signup_next_toSelectCategory_btn;
 
-    SignupPresenter signup_presenter
+    SignupPresenter signup_presenter;
 
 
     @Override
@@ -45,7 +46,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         signup_presenter = new SignupPresenter();
 
-        final boolean result_email, result_pw, result_username;
+        final boolean result_email, result_repw, result_username;
 
 
         signup_email_confirm_btn.setOnClickListener(new View.OnClickListener(){
@@ -59,8 +60,54 @@ public class SignUpActivity extends AppCompatActivity {
                     signup_email_confirm_btn.setBackground(R.drawable.check);
                 }
                 else{
-                    signup_email_confirm_btn.setBackgroundColor(R.drawable.confirm);
+                    signup_email_confirm_btn.setBackground(R.drawable.confirm);
+                    Toast.makeText(getApplicationContext(), "이미 존재하는 email입니다.",Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+        signup_repw_confirm_btn.setOnClickListener(new View.OnClickListener(){
+
+            String repw = signup_repw_edit.getText().toString();
+            @Override
+            public void onClick(View v) {
+                result_repw = signup_presenter.tryconfirmRepw(repw);
+                if(result_repw){
+                    //confirm아이콘 변화
+                    signup_repw_confirm_btn.setBackground(R.drawable.check);
+                }
+                else{
+                    signup_repw_confirm_btn.setBackground(R.drawable.confirm);
+                    Toast.makeText(getApplicationContext(), "비밀번호가 일치하지 않습니다.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        signup_username_confirm_btn.setOnClickListener(new View.OnClickListener(){
+
+            String username = signup_username_edit.getText().toString();
+            @Override
+            public void onClick(View v) {
+                result_username = signup_presenter.tryconfirmUsername(username);
+                if(result_username){
+                    //confirm아이콘 변화
+                    signup_username_confirm_btn.setBackground(R.drawable.check);
+                }
+                else{
+                    signup_username_confirm_btn.setBackground(R.drawable.confirm);
+                    Toast.makeText(getApplicationContext(), "이미 존재하는 username입니다.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        signup_next_toSelectCategory_btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                boolean result = signup_presenter.trySignup();
+                if (result) {
+                    startActivity(new Intent(getApplicationContext(), SignUpSelectCategoryActivity.class));
+                    finish();
+                }
+
             }
         });
 
---
+++ app/src/main/java/indivus/cosmos/SignUpSelectCategoryActivity.java
@@ -0,0 +1,18 @@
+package indivus.cosmos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2017-06-28.
+ */
+
+public class SignUpSelectCategoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_select_category);
+    }
+}
