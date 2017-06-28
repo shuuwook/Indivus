855e5c9e65695cdb7dca2c46c1e066e790c2c4c6
Jiyong-C-Lee <liramus214@gmail.com>
2017-06-28 16:42:05 +0900

    update
    update

--- app/src/main/java/indivus/cosmos/Splash.java
+++ app/src/main/java/indivus/cosmos/Splash.java
@@ -3,7 +3,6 @@ package indivus.cosmos;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 
 /**
@@ -16,7 +15,7 @@ public class Splash extends Activity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, Login.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
 
